6a4663583489f3d4028655117fdd577da001b240
Sanjivi <jsanjivi@hotmail.com>
2016-06-15 15:50:24 +0530

    commit 3

--- MockForm/app/src/main/java/com/example/sanjivi/mockform/MainActivity.java
+++ MockForm/app/src/main/java/com/example/sanjivi/mockform/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
@@ -14,16 +13,18 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Switch;
 import android.widget.Toast;
-
-import java.util.Date;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends AppCompatActivity implements OnItemSelectedListener {
-    Spinner spinner;
+
     String[] departments={"ARCHI","CHEM","CIVIL","CSE","ECE","EEE","ICE","MECH","META","PROD",};
     private CheckBox check1,check2,check3,check4;
-    String name;
+    String name,roll;
+    boolean tronix,webdev,appdev,algos,mentor;
+    int dept;
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +36,38 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
         spinner.setAdapter(adapter); //attaching adapter to spinner
         spinner.setOnItemSelectedListener(this); //spinner click listener
         spinner.setSelection(3);
+        if(savedInstanceState!=null) {
+            name =savedInstanceState.getString("name");
+            roll=savedInstanceState.getString("roll");
+            dept=savedInstanceState.getInt("dept");
+            tronix=savedInstanceState.getBoolean("tronix");
+            webdev=savedInstanceState.getBoolean("webdev");
+            appdev=savedInstanceState.getBoolean("appdev");
+            algos=savedInstanceState.getBoolean("algos");
+            mentor=savedInstanceState.getBoolean("mentor");
+            check1 = (CheckBox) findViewById(R.id.checkBox);
+            check2 = (CheckBox) findViewById(R.id.checkBox2);
+            check3 = (CheckBox) findViewById(R.id.checkBox3);
+            check4 = (CheckBox) findViewById(R.id.checkBox4);
+            EditText edit1 = (EditText) findViewById(R.id.editText);
+            EditText edit2 = (EditText) findViewById(R.id.editText2);
+            Switch toggleSwitch=(Switch)findViewById(R.id.switch1);
+            edit1.setText(name);
+            edit2.setText(roll);
+            spinner.setSelection(dept);
+            check1.setChecked(tronix);
+            check2.setChecked(webdev);
+            check3.setChecked(appdev);
+            check4.setChecked(algos);
+            toggleSwitch.setChecked(mentor);
+
+
+        }
         addListenerOnButton();
     }
     public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-     String item=arg0.getItemAtPosition(arg2).toString(); //on selecting a spinner item
+      String item=arg0.getItemAtPosition(arg2).toString(); //on selecting a spinner item
+        dept=arg2;
 
     }
     public void onNothingSelected(AdapterView<?> arg0)
@@ -56,13 +85,20 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
                check4 = (CheckBox) findViewById(R.id.checkBox4);
                EditText edit1 = (EditText) findViewById(R.id.editText);
                EditText edit2 = (EditText) findViewById(R.id.editText2);
+               Switch toggleSwitch=(Switch)findViewById(R.id.switch1);
+               mentor=toggleSwitch.isChecked();
                name = edit1.getText().toString();
-               String roll = edit2.getText().toString();
+               roll = edit2.getText().toString();
+               tronix=check1.isChecked();
+               webdev=check2.isChecked();
+               appdev=check3.isChecked();
+               algos=check4.isChecked();
+
                if (name.matches(""))
                    Toast.makeText(MainActivity.this, "Fill in your name", Toast.LENGTH_LONG).show();
                else if (roll.matches(""))
                    Toast.makeText(MainActivity.this, "Fill in your roll no", Toast.LENGTH_LONG).show();
-               else if (!check1.isChecked() && !check2.isChecked() && !check3.isChecked() && !check4.isChecked())
+               else if (!tronix && !webdev && !appdev && !algos)
                    Toast.makeText(MainActivity.this, "Fill in your profile", Toast.LENGTH_LONG).show();
                else {
 
@@ -75,5 +111,17 @@ public class MainActivity extends AppCompatActivity implements OnItemSelectedLis
        });
 
    }
+ @Override
+    protected void onSaveInstanceState(Bundle outState)
+ {outState.putString("name",name);
+     outState.putString("roll",roll);
+     outState.putInt("dept",dept);
+     outState.putBoolean("tronix",tronix);
+     outState.putBoolean("webdev",webdev);
+     outState.putBoolean("appdev",appdev);
+     outState.putBoolean("algos",algos);
+     outState.putBoolean("mentor",mentor);
 
+
+ }
 }
